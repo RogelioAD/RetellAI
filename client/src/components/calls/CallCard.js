71b38ef77f4bb1e63da8b0e1ef94bf290e875c63
Rogelio Acosta <rogeracosta2001@gmail.com>
2026-01-05 17:08:18 -0800

    Remove unused Button import from CallCard component to streamline code and improve maintainability.

--- client/src/components/calls/CallCard.js
+++ client/src/components/calls/CallCard.js
@@ -10,7 +10,6 @@ import {
 } from "../../utils/callDataTransformers";
 import { formatFullDate } from "../../utils/dateFormatters";
 import TranscriptView from "./TranscriptView";
-import Button from "../common/Button";
 import { cardStyles, gradients } from "../../constants/styles";
 
 /**
